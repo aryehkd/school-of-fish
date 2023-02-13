c8c869d44f53312682eb3c64312fcd1aa1fbf876
Aryeh Kempler-DeLugach <aryeh@Aryehs-MacBook-Pro.local>
2023-02-13 14:43:05 -0800

    add server package and nvmrc

--- client/src/components/image-upload/ImageUpload.tsx
+++ client/src/components/image-upload/ImageUpload.tsx
@@ -1,10 +1,27 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 
 // @ts-ignore
 export interface ImageUploadProps {}
 
 export const ImageUpload: FunctionComponent<ImageUploadProps> = (props) => {
-  return (<></>)
+  const [ image, setImage ] = useState(null)
+
+  const onFileChange = event => {
+    setImage(event.target.files[0]);
+  };
+
+  const onFileUpload = () => {
+    console.log(image);
+  };
+
+  return (
+    <div>
+        <input type="file" onChange={onFileChange} />
+        <button onClick={onFileUpload}>
+          Upload!
+        </button>
+    </div>
+  )
 }
 
 export default ImageUpload
